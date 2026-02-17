db2edfbd5a39bd8e856abc0c654851cb01c7df15
solitary22EE <rajkumarkarikkal8942@gmail.com>
2026-02-17 06:49:01 +0530

    change firebase config

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -4,14 +4,17 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA0xe7VzhqK0N_JDvWGUi4KfYL8EbOycXs",
-  authDomain: "neads-website.firebaseapp.com",
-  projectId: "neads-website",
-  storageBucket: "neads-website.appspot.com",
-  messagingSenderId: "484420778418",
-  appId: "1:484420778418:web:49ec17da11d80c4f294cab",
+  apiKey: "AIzaSyBTyUQaBM4_exX496nDjS72YIsmM3l5I0Y",
+  authDomain: "menu-admin-76356.firebaseapp.com",
+  databaseURL: "https://menu-admin-76356-default-rtdb.firebaseio.com",
+  projectId: "menu-admin-76356",
+  storageBucket: "menu-admin-76356.firebasestorage.app",
+  messagingSenderId: "882758954881",
+  appId: "1:882758954881:web:67a13abd6e6d0748670280",
+  measurementId: "G-5MFB75J8GZ"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
